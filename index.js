5b5f1708656ea4845388d8bb0a270515a42a62e0
Epsit <99855060+epsit03@users.noreply.github.com>
2023-07-30 02:38:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ alert(
   "The firebase database trial has now expired so, even if you try to add anything to the cart, it won't be shown on the screen since the database no longer gives permission to the web app. Any suggestions/improvements are most welcomed."
 );
 if(window.confirm("Wanna contribute in the project?")){
-  window.open("https://github.com/epsit03/add-to-cart");
+  window.open("https://github.com/epsit03/add-to-cart");}
   
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
